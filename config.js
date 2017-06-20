1463dfa5d9edd5abf98fba1386ec537f4ad42d46
Xavier <xcoadic@protonmail.com>
2017-06-20 15:15:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,8 @@ var GH = {}
 GH.SECRET = 'YWU0ZmRkZGFjOTVlZGM1ZTc1MmI3NjRjZTI3Y2UxZGYyMzdmZTdkZg=='
 GH.ID = 'ODUzMjY1YjA5YjBjMjVlOTg2MTQ='
 // Github Owner's
-GH.OWNER = 'LeBiome'
+GH.OWNER = 'OpenBioFabrics'
 // Github crew's repository
 GH.CREW = 'organisations'
 
-var MULTIBAO = {}
-MULTIBAO.UVP1 = 'Biomimicry Wide Community'
-MULTIBAO.UVP2 = 'DIY / DIT / DIWO'
-MULTIBAO.BUTTON1 = 'Découvrir'
-MULTIBAO.BUTTON2 = 'Être Biomer'
-MULTIBAO.BUTTON3 = 'Agenda'
+
